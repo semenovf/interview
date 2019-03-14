0c8fec621090644d8384318dceacc4e05ed5afe1
Fedor Semenov <fedor.v.semenov@gmail.com>
2019-03-14 09:51:31 +0500

    Added sort.insertion.

--- cxxlang/sort.insertion.cpp
+++ cxxlang/sort.insertion.cpp
@@ -10,28 +10,33 @@ void insertion_sort (BidirIter first, BidirIter last, Less less)
     using std::swap;
 
     BidirIter cur = first;
-    BidirIter x = cur;
-    ++x;
-
-    if (x == last)
-        return;
 
     while (cur != last) {
         BidirIter prev = cur;
+        BidirIter x = cur;
+        ++x;
 
-        while (prev != first && less(*x, *prev)) {
-            swap(*x, *prev);
-            --prev;
+        if (x == last)
+            break;
+
+        do {
+            if (less(*x, *prev))
+                swap(*x, *prev);
+
             --x;
-        }
+        } while (prev-- != first);
 
         ++cur;
     }
 }
 
 TEST_CASE ("Insertion sort", "algo.sort") {
+    std::array<int, 0> a0{};
+    std::array<int, 1> a1{10};
     std::array<int, 10> a{5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
 
+    insertion_sort(std::begin(a0), std::end(a0), std::less<int>{});
+    insertion_sort(std::begin(a1), std::end(a1), std::less<int>{});
     insertion_sort(std::begin(a), std::end(a), std::less<int>{});
 
     CHECK(a[0] == 0);
